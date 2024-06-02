4a638372ac7cf2cc16efa0bb7b81db633df940ac
Artem <panasuck@bigmir.net>
2024-06-02 23:58:25 +0300

    added loader and preloader

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,8 @@
+import { Container } from "pixi.js";
+
 export type TCoordinate = { x: number; y: number };
+
+export type TScene = new (stage: Container) => IGameScene;
+export interface IGameScene {
+  update(delta: number): void;
+}
